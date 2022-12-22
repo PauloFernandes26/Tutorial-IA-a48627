6f64f66cb721455459b90f4d186e0951b103b80b
PauloFernandes26 <ploupaulo@gmail.com>
2022-12-22 22:27:33 +0000

    Moving as One a48627

---
+++ Assets/AIController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    void Start() {
+
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+}
